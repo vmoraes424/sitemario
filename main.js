39d2be377db4f1e3d3912dba52be46158b4b5a2e
vmoraes424 <vmoraes424@gmail.com>
2023-05-16 11:33:11 -0300

    Adicionando Pulo, animação e cano

--- main.js
+++ main.js
@@ -2,37 +2,52 @@ const mario = document.querySelector("#mario");
 const logo = document.querySelector("#logo");
 const coin = document.querySelector("#coin");
 
-document.addEventListener("keypress", (e) => {
-  if (e.code === "Space") {
-    e.preventDefault();
-    mario.style.animation = "none";
-    void mario.offsetWidth; // reflow
-    mario.style.animation = "mario .6s normal";
-  }
-});
-
 logo.addEventListener("click", (e) => {
   logo.style.animation = "none";
-  void logo.offsetWidth; // reflow
+  void logo.offsetWidth;
   logo.style.animation = "logo .25s ease";
   coin.style.animation = "none";
-  void coin.offsetWidth; // reflow
+  void coin.offsetWidth;
   coin.style.animation = "coin .5s ease";
 });
 
 let posicaoAtual = 0;
-let velocidade = 15
+let alturaPulo = 150;
+let velocidade = 15;
+let teclaPressionada = false;
+let pulando = false;
 
-document.addEventListener('keydown', (e) => {
-  if (e.code === 'KeyD') {
-    posicaoAtual += velocidade
+document.addEventListener("keydown", (e) =>  {
+  if (e.code === "KeyA") {
+    posicaoAtual -= velocidade;
+    teclaPressionada = true
+    mario.style.transform = `translateX(${posicaoAtual}px) rotateY(180deg)`;
+    mario.src = "/resources/css/img/mairocorrendo.gif";
+    mario.style.width = "120px";
+  }
+  
+  if (e.code === "KeyD") {
+    posicaoAtual += velocidade;
+    teclaPressionada = true
     mario.style.transform = `translateX(${posicaoAtual}px)`;
+    mario.src = "/resources/css/img/mairocorrendo.gif";
+    mario.style.width = "120px";
+  }
+  if (e.code === "Space") {
+    e.preventDefault();
+    mario.animate(
+      [{ bottom: `60px;` }, { bottom: `${alturaPulo}px` }, { bottom: `60px;` }],
+      {
+        duration: 300,
+      }
+    );
   }
 });
-document.addEventListener('keydown', (e) => {
-  if (e.code === 'KeyA') {
-    posicaoAtual -= velocidade
-    mario.style.transform = `translateX(${posicaoAtual}px) rotateY(180deg)`;
-    mario.style.transition = "translateX .3s ease"
+
+document.addEventListener("keyup", (e) => {
+  if (e.code === "KeyD" || e.code === "KeyA") {
+    teclaPressionada = false;
+    mario.src = "/resources/css/img/mario.png";
+    mario.style.width = "100px";
   }
 });
